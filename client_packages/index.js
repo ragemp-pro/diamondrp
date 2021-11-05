a274814e42c93ae44d94f700b99ba2d210343b1c
gerodox-dev <quarpadd@gmail.com>
2021-11-06 00:42:13 +0300

    Update

--- client_packages/index.js
+++ client_packages/index.js
@@ -1,6 +1,13 @@
-
 try {
-    require('./client.js');
-} catch(e) {
-    mp.game.graphics.notify(e.toString());
-}
+    //require('./main.js');
+    require('./client/index.js');
+  } catch (e) {
+    mp.game.graphics.notify(`${e.toString()}`);
+  }
+  let enabledTestMod = false
+  mp.events.add("server:test", (beta = false) => {
+    if(enabledTestMod) return;
+    enabledTestMod = true;
+    if(!beta)require('_rage-console');
+  })
+  
